62dc2fc0d25cd747a936dfe12f3908e41ca2baf9
rajchawan <rajm3chawan@gmail.com>
2025-05-19 00:03:15 +0530

    Revert "Exception Handling"
    This reverts commit 992268e253855f8e3703afc1b055a9912c809de2.
    
    t

---
+++ Vehicle.java
@@ -0,0 +1,10 @@
+public class Vehicle {
+  String color;
+  String horsePower;
+  String manufacturer;
+  int tankCapacity;
+
+  public static void greet() {
+    System.out.println("Greetings from Base Class");
+  }
+}
